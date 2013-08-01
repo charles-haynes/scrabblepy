99783f4217b9db621c685386c2ec0fc7f13c40c1
Charles Haynes <haynes+git@biz.fifth-mountain.com>
2013-08-01 09:22:08 +0800

    cleanup score to be more readable

--- score.py
+++ score.py
@@ -5,11 +5,12 @@ __author__ = 'chaynes'
 import re
 import sys
 
-blank_pattern="\([^)]*\)|[A-Z]"
+blank_pattern = "\([^)]*\)|[A-Z]"
 
 letters = {'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8, 'k': 5, 'l': 1, 'm': 3,
            'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10,
            '.': 0}
+
 
 def score(word):
     """
@@ -20,19 +21,20 @@ def score(word):
     :rtype: int
     """
 
-    word_length = len(re.sub("[()]", "", word))
+    word_length_with_blanks = len(re.sub("[()]", "", word))
+    word_minus_blanks = re.sub(blank_pattern, "", word)
+    letter_value = sum(letters[letter] for letter in word_minus_blanks)
+    return letter_value + 50 if word_length_with_blanks >= 7 else letter_value
 
-    letter_value = sum(letters[letter] for letter in re.sub(blank_pattern, "", word))
-    return letter_value + 50 if word_length >= 7 else letter_value
 
 def main():
     """
     run the main program
 
     """
-    for line in sys.stdin:
-        line = line.strip("\n\r")
+    for line in sys.stdin.read().split():
         print score(line), line
+
 
 if __name__ == "__main__":
     main()
