121679b1c4d05792a25db88daa3cdb73eafa3486
Charles Haynes <haynes+git@biz.fifth-mountain.com>
2014-02-23 17:26:05 +1000

    working

---
+++ scrabbleController.js
@@ -0,0 +1,5 @@
+var scrabbleAppModule = angular.module('scrabbleApp', []);
+
+scrabbleAppModule.controller('ScrabbleController', function($scope) {
+    $scope.words = ['aa','ab','qi','xi','xin'];
+});
